b013a9d53dfab2a2f1434a43067b1f56162eda94
mrom <mrom@posteo.de>
2020-10-14 23:21:01 +0200

    Refactor record handling into functions

--- covid.py
+++ covid.py
@@ -1,33 +1,50 @@
+# Notes:
+# - For piping to jq, use json.dumps
+
 import requests, json
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Notes:
-# - For piping to jq, use json.dumps
+# TODO: Improve data acquision time
+# TODO: Refactor completely into functions
+# TODO: Get last date from file / records
+# TODO: Add second y-axis in histogram
+# TODO: Add smoothing
 
-# with open("covid.json", "w") as outfile:
-#     response = requests.get("https://opendata.ecdc.europa.eu/covid19/casedistribution/json/")
-#     response.raise_for_status()
-#     d = json.loads(response.text)
-#     json.dump(d, outfile)
 
-with open("covid.json", "r") as infile:
-    d = json.load(infile)
-records = d['records']
-print('%d Einträge vorhanden' % len(records))
+def save_data_to_file(filename):
+    with open(filename, "w") as outfile:
+        response = requests.get("https://opendata.ecdc.europa.eu/covid19/casedistribution/json/")
+        print(f"HTTP GET - Status-Code: {response.status_code}")
+        d = json.loads(response.text)
+        json.dump(d, outfile)
 
-def nice(r):
+def get_records_from_file(filename):
+    with open("covid.json", "r") as infile:
+        d = json.load(infile)
+    records =  d['records']
+    print('%d Einträge vorhanden' % len(records))
+    return records
+
+def nice(record):
     return {
-        "Land": r['geoId'], 
-        "Datum": r['dateRep'], 
-        "Fälle": r['cases'],
-        "Todesfälle": r['deaths']
+        "Land": record['geoId'], 
+        "Datum": record['dateRep'], 
+        "Fälle": record['cases'],
+        "Todesfälle": record['deaths']
     }
 
-records = [ nice(r) for r in records ]
-print('%d Einträge für vorhanden' % len(records))
-records = [ r for r in records if r['Land']=='DE' ]
-print('%d Einträge für DE vorhanden' % len(records))
+def get_nice_records_from_DE(filename):
+    records = get_records_from_file(filename)
+    records = [ nice(r) for r in records ]
+    records = [ r for r in records if r['Land']=='DE' ]
+    print('%d Einträge für DE vorhanden' % len(records))
+    return records
+
+
+#save_data_to_file("covid.json")
+records = get_nice_records_from_DE("covid.json")
+
 #print(json.dumps(records[:5], indent=4))
 cases = [ records[i]['Fälle'] for i in range(len(records))]
 deaths = [ records[i]['Todesfälle'] for i in range(len(records))]
